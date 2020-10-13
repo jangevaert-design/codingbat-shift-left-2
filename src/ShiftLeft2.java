a9c4e9aeea6b1b8682c993d9f81476e39e69d423
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-13 06:16:12 -0600

    Initial commit

---
+++ src/ShiftLeft2.java
@@ -0,0 +1,11 @@
+public class ShiftLeft2 {
+  public int[] shiftLeft(int[] nums) {
+
+    for (int i = 0; i < nums.length - 1; i++) {
+      int temp = nums[i];
+      nums[i] = nums[i + 1];
+      nums[i + 1] = temp;
+    }
+    return nums;
+  }
+}
